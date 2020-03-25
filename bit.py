c13dca1b251635b42e36fbe711a6158fc29a66de
alifsamudra74 <58199331+alifsamudra74@users.noreply.github.com>
2020-03-25 15:47:20 +0700

    Update bit.py
    Error class for bit.py
    
    episode 1 minute 8:15

--- bit.py
+++ bit.py
@@ -5,6 +5,19 @@
 DIGITS = '0123456789'
 
 #######################
+#        ERRORS
+#######################
+
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f'{self.error_name}: {self.details}'
+        return result
+
+#######################
 #        TOKENS
 #######################
 
