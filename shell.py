3cf75d18262969f339aee4e692cc04b9d671debf
alifsamudra74 <58199331+alifsamudra74@users.noreply.github.com>
2020-03-26 13:21:25 +0700

    Update to bit.py and shell.py
    Character position tracker

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import bit
 
 while True:
     text = input('bit > ')
-    result, error = bit.run(text)
+    result, error = bit.run('<stdin>', text)
 
     if error: print(error.as_string())
     else: print(result)
