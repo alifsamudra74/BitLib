6f406d30702b01140be1b492ccef3508e8225c32
alifsamudra74 <58199331+alifsamudra74@users.noreply.github.com>
2020-03-26 13:51:51 +0700

    Update to bit.py
    A few bug fixes

--- bit.py
+++ bit.py
@@ -14,14 +14,14 @@ class Error:
         self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
-
+    
     def as_string(self):
-        result = f'{self.error_name}: {self.details}'
+        result  = f'{self.error_name}: {self.details}\n'
         result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
 
 class IllegalCharError(Error):
-    def __init__(self, pos_start, pos_end,  details):
+    def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 #######################
@@ -53,7 +53,7 @@ class Position:
 #        TOKENS
 #######################
 
-TT_INT      = 'INT'
+TT_INT		= 'INT'
 TT_FLOAT    = 'FLOAT'
 TT_PLUS     = 'PLUS'
 TT_MINUS    = 'MINUS'
@@ -66,7 +66,7 @@ class Token:
     def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
-
+    
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
@@ -82,7 +82,7 @@ class Lexer:
         self.pos = Position(-1, 0, -1, fn, text)
         self.current_char = None
         self.advance()
-        
+    
     def advance(self):
         self.pos.advance(self.current_char)
         self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
@@ -97,22 +97,28 @@ class Lexer:
                 tokens.append(self.make_number())
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
+                self.advance()
             elif self.current_char == '-':
                 tokens.append(Token(TT_MINUS))
+                self.advance()
             elif self.current_char == '*':
                 tokens.append(Token(TT_MUL))
+                self.advance()
             elif self.current_char == '/':
                 tokens.append(Token(TT_DIV))
+                self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN))
+                self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(TT_RPAREN))
+                self.advance()
             else:
                 pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
                 return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
-            
+
         return tokens, None
 
     def make_number(self):
@@ -124,9 +130,9 @@ class Lexer:
                 if dot_count == 1: break
                 dot_count += 1
                 num_str += '.'
-                else:
-                    num_str += self.current_char
-                self.advance()
+            else:
+                num_str += self.current_char
+            self.advance()
 
         if dot_count == 0:
             return Token(TT_INT, int(num_str))
