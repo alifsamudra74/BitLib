fe8abe0f7415d3a594f6d001d150dad6368b344e
alifsamudra74 <58199331+alifsamudra74@users.noreply.github.com>
2020-04-01 16:55:36 +0700

    Update bit.py
    bin_op for expr & term, ep 2 8.31

--- bit.py
+++ bit.py
@@ -164,29 +164,46 @@ class BinOpNode:
 #######################
 
 class Parser:
-    def __init__(tokens):
+    def __init__(self, tokens):
         self.tokens = tokens
         self.tok_idx = 1
         self.advance()
     
-    def advance():
+    def advance(self):
         self.tok_idx += 1
         if self.tok_idx < len(self.tokens):
             self.current_tok = self.tokens[self.tok_idx]
         return self.current_tok
 
-    def factor():
+    #######################
+
+    def parse(self):
+        res = self.expr()
+        return res
+
+    def factor(self):
         tok = self.current_tok
 
         if tok.type in (TT_INT, TT_FLOAT):
             self.advance()
             return NumberNode(tok)
 
-    def term():
-        pass
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
 
-    def expr():
-        pass
+    def expr(self):
+        return self.bin_op(self.factor, (TT_PLUS, TT_MINUS))
+    
+    def bin_op(self, func, ops):
+        left = func()
+
+        while self.current_tok in ops:
+            op_tok = self.current_tok
+            self.advance()
+            right = func()
+            left = BinOpNode(left, op_tok, right)
+        
+        return left
 
 #######################
 #         RUN
