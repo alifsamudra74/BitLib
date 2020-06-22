863f4d905ffbcfb55c400fbe3d18669274a67d54
alifsamudra74 <58199331+alifsamudra74@users.noreply.github.com>
2020-06-22 11:20:03 +0700

    END

--- shell.py
+++ shell.py
@@ -1,8 +1,14 @@
 import bit
 
 while True:
-    text = input('bit > ')
-    result, error = bit.run('<stdin>', text)
+	text = input('bit > ')
+	if text.strip() == "": continue
+	result, error = bit.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
